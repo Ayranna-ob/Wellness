2669f6eb151e460e7b0474178678a948c65d5531
Ayranna Kugathas <ayrannakugathas@ayrannas-mbp.home>
2025-02-13 16:33:33 +0000

    latest code

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Homepage2 from "./pages/Homepage2";
+
+function App() {
+    return (
+        <div>
+            <Homepage2 />
+        </div>
+    );
+}
+
+export default App;
